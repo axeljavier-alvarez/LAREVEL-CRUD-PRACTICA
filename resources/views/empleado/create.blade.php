b65cc26782187b3b10e8a7614a02a85c9c1d1cd1
Axel Alvarez <axjavier4alvarez44@gmail.com>
2025-10-27 22:04:09 -0600

    crud larevel con migraciones ver, agregar y eliminar

---
+++ resources/views/empleado/create.blade.php
@@ -0,0 +1,19 @@
+<form action="{{ url('/empleado') }}" method="post" enctype="multipart/form-data">
+    @csrf
+    <label for="Nombre">Nombre</label>
+    <input type="text" name="Nombre" id="Nombre">
+    <br>
+    <label for="ApellidoPaterno">Apellido Paterno</label>
+    <input type="text" name="ApellidoPaterno" id="ApellidoPaterno">
+    <br>
+    <label for="ApellidoMaterno">Apellido Materno</label>
+    <input type="text" name="ApellidoMaterno" id="ApellidoMaterno">
+    <br>
+    <label for="Correo">Correo</label>
+    <input type="email" name="Correo" id="Correo">
+    <br>
+    <label for="Foto">Foto</label>
+    <input type="file" name="Foto" id="Foto">
+    <br>
+    <input type="submit" value="Guardar datos">
+</form>
---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EmpleadoController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+/*
+Route::get('/empleado', function(){
+    return view('empleado.index');
+});
+
+Route::get('/empleado/create', [EmpleadoController::class, 'create']);
+*/
+
+// acceder a todas las rutas
+Route::resource('empleado', EmpleadoController::class);
+
